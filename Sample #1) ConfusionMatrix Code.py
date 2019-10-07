0f60193cca1a94ae4fddfbe3f7346d9dc73e69f0
POORNA CHAND <46214838+VangaPoornaChand@users.noreply.github.com>
2019-10-07 14:19:07 +0530

    Code For Confusion Matrix Uploaded

---
+++ Sample #1) ConfusionMatrix Code.py
@@ -0,0 +1,19 @@
+cm = metrics.confusion_matrix(y_test, predictions)
+
+plt.figure(figsize=(9,9))
+plt.imshow(cm, interpolation='nearest', cmap='Pastel1')
+plt.title('Confusion matrix', size = 15)
+plt.colorbar()
+tick_marks = np.arange(10)
+plt.xticks(tick_marks, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], rotation=45, size = 10)
+plt.yticks(tick_marks, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], size = 10)
+plt.tight_layout()
+plt.ylabel('Actual label', size = 15)
+plt.xlabel('Predicted label', size = 15)
+width, height = cm.shape
+
+for x in range(width):
+ for y in range(height):
+  plt.annotate(str(cm[x][y]), xy=(y, x), 
+  horizontalalignment='center',
+  verticalalignment='center')
